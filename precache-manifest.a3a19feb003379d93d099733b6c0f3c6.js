70b5b145a11b0709e17d5ca1bf44333ae71a59b7
DosImpact <ypd03008@gmail.com>
2020-05-12 17:33:00 +0900

    Updates

---
+++ precache-manifest.a3a19feb003379d93d099733b6c0f3c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7244dd6c649830726d58be439fe9a024",
+    "url": "/blockCrawl_frontend/index.html"
+  },
+  {
+    "revision": "1e0f3ba1cb0cf14bd1a4",
+    "url": "/blockCrawl_frontend/static/js/2.5de96d68.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/blockCrawl_frontend/static/js/2.5de96d68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5ce71aa1e90aed87d26",
+    "url": "/blockCrawl_frontend/static/js/main.fb392b7a.chunk.js"
+  },
+  {
+    "revision": "fcf7fa5db8a413746aa1",
+    "url": "/blockCrawl_frontend/static/js/runtime-main.553627cc.js"
+  }
+]);
